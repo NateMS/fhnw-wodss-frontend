715ba267136f305cd74551e7184a5cfe6be16504
Kelvin Louis <me@kelvinlouis.com>
2019-04-09 09:22:14 +0200

    Remove temporary fix

--- src/api/dto/contract.base.model.ts
+++ src/api/dto/contract.base.model.ts
@@ -8,27 +8,13 @@ export class ContractBaseModel implements Contract {
 
   constructor(contract: Contract) {
     if (contract.startDate != null) {
-      if (Array.isArray(contract.startDate)) {
-        // TODO Remove once backend fixes it
-        this.startDate = contract.startDate
-          .map(num => num < 10 ? `0${num}` : `${num}`)
-          .join('-');
-      } else {
-        this.startDate = contract.startDate;
-      }
+      this.startDate = contract.startDate;
     } else {
       throw new Error(`The field 'startDate' is missing.`);
     }
 
     if (contract.endDate != null) {
-      if (Array.isArray(contract.endDate)) {
-        // TODO Remove once backend fixes it
-        this.endDate = contract.endDate
-          .map(num => num < 10 ? `0${num}` : `${num}`)
-          .join('-');
-      } else {
-        this.endDate = contract.endDate;
-      }
+      this.endDate = contract.endDate;
     } else {
       throw new Error(`The field 'endDate' is missing.`);
     }
